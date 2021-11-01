974924d11139e71bea1d2410d1ce207dc114e7aa
Mahesh Adhikari <62585493+mahesh-gfx@users.noreply.github.com>
2021-11-01 15:11:17 +0530

    Added weather update on Live location and search

---
+++ scripts/app.js
@@ -0,0 +1,69 @@
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+//Geolocation Api
+navigator.geolocation.getCurrentPosition(success, error, options);
+
+function success(pos) {
+  var latitude = pos.coords.latitude;
+  var longitude = pos.coords.longitude;
+
+  var accessToken = "pk.d2a483ead28060cc7405107f8b3e40de";
+  var revGeocodingUrl = `https://us1.locationiq.com/v1/reverse.php?key=${accessToken}&lat=${latitude}&lon=${longitude}&format=json`;
+
+  var loc = "";
+
+  //Reverse geolocation API call
+  fetch(revGeocodingUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then((data) => {
+      loc += data.address.county;
+      loc += ", ";
+      loc += data.address.country;
+      weatherAPI(loc);
+    });
+}
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+function weatherAPI(location) {
+  var url = `http://api.weatherapi.com/v1/current.json?key=981c56aab5ba4484a7a105826210109&q=${location}`;
+  //Weather API Call
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then((data) => {
+      var date = new Date(data.current.last_updated);
+      document.getElementById("condition-ico").src =
+        data.current.condition.icon;
+      document.getElementById("location").innerHTML = location;
+      document.getElementById("temperature").innerHTML = data.current.temp_c;
+      document.getElementById("humidity").innerHTML =
+        " " + data.current.humidity;
+      document.getElementById("precipitation").innerHTML =
+        " " + data.current.precip_mm + " mm";
+      document.getElementById("last-update").innerHTML =
+        date.toDateString() + " at " + date.toLocaleTimeString();
+      document.getElementById("condition").innerHTML =
+        data.current.condition.text + " at";
+      document.getElementById("wind").innerHTML =
+        data.current.wind_kph +
+        " kph in " +
+        data.current.wind_dir +
+        " direction.";
+      document.getElementById("pressure").innerHTML =
+        " " + data.current.pressure_mb + " millibars";
+    });
+}
+
+document.getElementById("search-query").onclick = function search() {
+  var searchQuery = document.getElementById("search").value;
+  weatherAPI(searchQuery);
+};
